fa950f232c2d38c0893ca2f923bd401c146e6653
apo360.20 <apo360.20@gmail.com>
2024-02-12 13:50:51 +0100

    update vendor

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,13 +17,22 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('two_factor_secret')->nullable();
+            $table->text('two_factor_recovery_codes')->nullable();
+            $table->timestamp('two_factor_confirmed_at')->nullable();
             $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
+            $table->unsignedBigInteger('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
-            $table->string('CompanyID', 50);
             $table->timestamps();
+            $table->enum('status', ['Ativo', 'Desativado', 'Pendente'])->default('Ativo');
+            $table->dateTime('password_expired')->nullable();
+            $table->tinyInteger('password_change_required')->default(0);
+            $table->dateTime('last_access')->nullable();
+            $table->unsignedBigInteger('role_id'); // Alterado para seguir convenção snake_case
+            $table->unsignedInteger('empresa_id'); // Alterado para seguir convenção snake_case
 
-            $table->foreign('CompanyID')->references('CompanyID')->on('Header');
+            $table->foreign('role_id')->references('id')->on('roles');
+            $table->foreign('empresa_id')->references('id')->on('empresa');
         });
     }
 
